1824c5237b153c286cb2a71586083fa6a004cb34
kholoud001 <kholoud.sanak@gmail.com>
2023-11-17 14:47:41 +0100

    page 2 font

--- delete_annonce.php
+++ delete_annonce.php
@@ -28,6 +28,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["delete_annonce"])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
     <title>Dashboard</title>
 </head>
 <body>
@@ -35,13 +36,30 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["delete_annonce"])) {
 <h2>Welcome to the Dashboard</h2>
 
 <!-- Delete Annonce Form -->
-<h3>Delete Annonce</h3>
-<form method="post" action="delete_annonce.php">
-    <label for="annonce_id_to_delete">Annonce ID to Delete:</label>
-    <input type="text" name="annonce_id_to_delete" required>
 
-    <input type="submit" name="delete_annonce" value="Delete Annonce">
-</form>
+
+<div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+    <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+        <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Delete Annonce</h2>
+    </div>
+
+    <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <form class="space-y-6" action="<?php echo $_SERVER["PHP_SELF"]; ?>" method="POST">
+            <div>
+                <label for="annonce_id_to_delete"  class="block text-sm font-medium leading-6 text-gray-900">Annonce ID to Delete </label>
+                <div class="mt-2">
+                    <input  name="annonce_id_to_delete" type="text" autocomplete="annonce_id_to_delete" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                </div>
+            </div>
+            <div>
+                <button type="submit" name="delete_annonce" value="Delete Annonce" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Submit</button>
+            </div>
+
+        </form>
+
+</div>
+
+
 <p><a href="index.php">Back to Home</a></p>
 </body>
 </html>
