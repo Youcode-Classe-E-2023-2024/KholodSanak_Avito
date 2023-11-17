4e0a25da9e37645648d2e57be5f5e7cb681f935c
kholoud001 <kholoud.sanak@gmail.com>
2023-11-17 14:47:51 +0100

    page 3 font

--- modify_annonce.php
+++ modify_annonce.php
@@ -32,6 +32,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["modify_annonce"])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
     <title>Dashboard</title>
 </head>
 <body>
@@ -39,22 +40,47 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["modify_annonce"])) {
 <h2>Welcome to the Dashboard</h2>
 
 <!-- Modify Annonce Form -->
-<h3>Modify Annonce</h3>
-<form method="post" action="modify_annonce.php">
-    <label for="annonce_id">Annonce ID to Modify:</label>
-    <input type="text" name="annonce_id" required>
 
-    <label for="new_title">New Title:</label>
-    <input type="text" name="new_title" required>
 
-    <label for="new_description">New Description:</label>
-    <textarea name="new_description" required></textarea>
+<div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+    <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+        <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Modify Annonce</h2>
+    </div>
 
-    <label for="new_image">New Image URL:</label>
-    <input type="text" name="new_image" required>
+    <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <form class="space-y-6" action="<?php echo $_SERVER["PHP_SELF"]; ?>" method="POST">
+            <div>
+                <label for="annonce_id"  class="block text-sm font-medium leading-6 text-gray-900">Annonce ID to Modify </label>
+                <div class="mt-2">
+                    <input  name="annonce_id" type="text" autocomplete="annonce_id" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                </div>
+            </div>
+            <div>
+                <label for="new_title"  class="block text-sm font-medium leading-6 text-gray-900">New Title</label>
+                <div class="mt-2">
+                    <input  name="new_title" type="text" autocomplete="new_title" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                </div>
+            </div>
+            <div>
+                <label for="new_description"  class="block text-sm font-medium leading-6 text-gray-900"> New Description</label>
+                <div class="mt-2">
+                    <input  name="new_description" type="new_description" autocomplete="new_description" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                </div>
+            </div>
+            <div>
+                <label for="new_image"  class="block text-sm font-medium leading-6 text-gray-900">New Image URL</label>
+                <div class="mt-2">
+                    <input  name="new_image" type="text" autocomplete="new_image" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                </div>
+            </div>
 
-    <input type="submit" name="modify_annonce" value="Modify Annonce">
-</form>
+            <div>
+                <button type="submit" name="modify_annonce" value="Modify Annonce" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Submit</button>
+            </div>
+        </form>
+
+    </div>
+
 <p><a href="index.php">Back to Home</a></p>
 </body>
 </html>
